4969ea2abf9ffe54291103ab420a5cc5ae94c6c1
KARAN CHAVDA <kchavda944@gmail.com>
2025-09-09 15:29:33 +0000

    can you add white background in pwa logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -12,6 +12,7 @@ export function Logo({ className }: { className?: string }) {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
+      <circle cx="12" cy="12" r="12" fill="white" />
       <path d="M20.13 10.39 12 15l-8.13-4.61a2 2 0 0 1-1-1.73V7.34a2 2 0 0 1 1-1.73L12 1l8.13 4.61a2 2 0 0 1 1 1.73v4.02a2 2 0 0 1-1 1.73z"></path>
       <path d="m5.87 7.34 6.13 3.48 6.13-3.48"></path>
       <path d="M12 22V15"></path>
