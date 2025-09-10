61860de9d72241e9fd548c3aec075f221ce0f4ad
KARAN CHAVDA <kchavda944@gmail.com>
2025-09-10 07:36:40 +0000

    also i need splash screen for pwa and i have seen one pwa logo issue as

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -6,15 +6,16 @@ export function Logo({ className }: { className?: string }) {
       className={className}
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M20.13 10.39 12 15l-8.13-4.61a2 2 0 0 1-1-1.73V7.34a2 2 0 0 1 1-1.73L12 1l8.13 4.61a2 2 0 0 1 1 1.73v4.02a2 2 0 0 1-1 1.73z"></path>
-      <path d="m5.87 7.34 6.13 3.48 6.13-3.48"></path>
-      <path d="M12 22V15"></path>
+      <rect width="24" height="24" rx="4" fill="hsl(var(--primary))" />
+      <g stroke="hsl(var(--primary-foreground))">
+        <path d="M20.13 10.39 12 15l-8.13-4.61a2 2 0 0 1-1-1.73V7.34a2 2 0 0 1 1-1.73L12 1l8.13 4.61a2 2 0 0 1 1 1.73v4.02a2 2 0 0 1-1 1.73z"></path>
+        <path d="m5.87 7.34 6.13 3.48 6.13-3.48"></path>
+        <path d="M12 22V15"></path>
+      </g>
     </svg>
   );
 }
